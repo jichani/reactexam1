46ce72458cdf69492ee88eacf4c8e9054c229d91
jichani <jichan7571@naver.com>
2023-07-14 16:53:24 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+// import './App.css';
+
+import Myheader from './MyHeader';
 
 function App() {
   let name = "지찬";
 
+  const number = 5;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+      <Myheader />
+      <h2>안녕 리액트 {name}</h2>
+      <b id='bold_text'>
+        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
+      </b>
     </div>
   );
 }
